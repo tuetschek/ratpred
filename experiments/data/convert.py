b223118eeecf29ffbd73cdea5a0e1d1524e7d67c
Ondrej Dusek <o.dusek@hw.ac.uk>
2016-12-13 18:10:39 +0000

    Alowing medians & concatenating references

--- experiments/data/convert.py
+++ experiments/data/convert.py
@@ -15,6 +15,15 @@ def convert(args):
     log_info("Loading %s..." % args.input_file)
     data = pd.read_csv(args.input_file)
     log_info("Contains %d instances." % len(data))
+    if args.median:
+        log_info("Computing medians...")
+        group_cols = list(set(data.columns) - set(['informativeness', 'naturalness',
+                                                  'quality', 'judge']))
+        data = data.groupby(group_cols, as_index=False).median()
+    if args.concat_refs:
+        log_info("Concatenating all references for the same outputs...")
+        group_cols = list(set(data.columns) - set(['orig_ref']))
+        data = data.groupby(group_cols, as_index=False).agg(lambda vals: ' <|> '.join(vals))
     if args.shuffle:
         log_info("Shuffling...")
         data = data.iloc[np.random.permutation(len(data))]
@@ -44,6 +53,10 @@ if __name__ == '__main__':
                     help='Train-devel-test labels')
     ap.add_argument('-s', '--shuffle', action='store_true',
                     help='Shuffle data before dividing?')
+    ap.add_argument('-m', '--median', action='store_true',
+                    help='Group human ratings and use medians')
+    ap.add_argument('-c', '--concat_refs', action='store_true',
+                    help='Join and concatenate all references?')
     ap.add_argument('input_file', type=str, help='Path to the input file')
     ap.add_argument('output_dir', type=str,
                     help='Output directory (where train,devel,test TSV will be created)')
