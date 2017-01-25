d01465de20eb617cb1fed9884dee18d3f2fb8333
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-01-25 17:54:33 +0000

    Describing no hyp encoder

--- util/describe_experiment.pl
+++ util/describe_experiment.pl
@@ -77,6 +77,7 @@ $nn_shape .= ' +ce'  if ( $config_data =~ /'char_embs'\s*:\s*True/ );
 $nn_shape .= ' +reuse'  if ( $config_data =~ /'reuse_embeddings'\s*:\s*True/ );
 $nn_shape .= ' +da_enc'  if ( $config_data =~ /'da_enc'\s*:\s*True/ );
 $nn_shape .= ' -ref_enc'  if ( $config_data =~ /'ref_enc'\s*:\s*False/ );
+$nn_shape .= ' -hyp_enc'  if ( $config_data =~ /'hyp_enc'\s*:\s*False/ );
 $nn_shape .= ' +1/2s'  if ( $config_data =~ /'predict_halves'\s*:\s*True/ );
 $nn_shape .= ' +co-t'  if ( $config_data =~ /'predict_coarse'\s*:\s*'train'/ );
 $nn_shape .= ' +co-e'  if ( $config_data =~ /'predict_coarse'\s*:\s*'test'/ );
