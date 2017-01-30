4a4743847e9f98c9878c58d96d229e550c603708
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-01-30 19:36:39 +0000

    Allow DA+text data for fake training data

--- experiments/data/convert_oneperline_data.py
+++ experiments/data/convert_oneperline_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import codecs
 from argparse import ArgumentParser
+from tgen.data import DA
 
 
 def lines_to_list(file_name):
@@ -16,23 +17,35 @@ def lines_to_list(file_name):
 
 def convert(args):
     src = lines_to_list(args.src_file)
-    sys = lines_to_list(args.sys_file)
+    if args.das:
+        src = [DA.parse(da_text).to_cambridge_da_string() for da_text in src]
     ref = lines_to_list(args.ref_file)
-    score = [float(score) for score in lines_to_list(args.score_file)]
-    df = pd.DataFrame.from_dict({'mr': src,
-                                 'system_ref': sys,
-                                 'orig_ref': ref,
-                                 'quality': score})
-    df.to_csv(args.out_file, columns=['mr', 'system_ref', 'orig_ref', 'quality'],
-              sep=b"\t", index=False, encoding='UTF-8')
+    columns = ['mr', 'orig_ref']
+    df = pd.DataFrame.from_dict({'mr': src, 'orig_ref': ref})
+
+    if args.system_output:
+        sys = lines_to_list(args.system_output)
+        df['system_ref'] = sys
+        columns.append('system_ref')
+
+    if args.score:
+        score = [float(score) for score in lines_to_list(args.score)]
+        df['quality'] = score
+        columns.append('quality')
+
+    df.to_csv(args.out_file, columns=columns, sep=b"\t", index=False, encoding='UTF-8')
 
 
 if __name__ == '__main__':
     ap = ArgumentParser()
+    ap.add_argument('-d', '--das', action='store_true',
+                    help='Treat sources as Alex-style DAs, store Cambridge-style DAs' +
+                    'on the output')
+    ap.add_argument('-o', '--system-output', type=str, default=None,
+                    help='Path to system output data file')
+    ap.add_argument('-s', '--score', type=str, help='Path to score data file')
     ap.add_argument('src_file', type=str, help='Path to source data file')
-    ap.add_argument('sys_file', type=str, help='Path to system output data file')
-    ap.add_argument('ref_file', type=str, help='Path to reference (PE) data file')
-    ap.add_argument('score_file', type=str, help='Path to score data file')
+    ap.add_argument('ref_file', type=str, help='Path to reference/post-edit data file')
     ap.add_argument('out_file', type=str, help='Output TSV file')
 
     args = ap.parse_args()
