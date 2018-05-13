8afb94f0bd8f1e4f39d73d7acce2b13f1ac8f107
Ondrej Dusek <o.dusek@hw.ac.uk>
2018-05-13 18:03:57 +0100

    Improved E2E data conversion

--- experiments/input/convert_e2e.py
+++ experiments/input/convert_e2e.py
@@ -11,6 +11,10 @@ from tgen.data import DA
 from tgen.debug import exc_info_hook
 import sys
 import itertools
+import random
+import os
+import codecs
+import pprint
 
 sys.excepthook = exc_info_hook
 
@@ -57,13 +61,17 @@ def convert(args):
     data = []
     src_col = args.column
     trg_col = COLUMN_MAP[src_col[:3]]
+    unique_mrs = set()
+
     for _, src_inst in src.iterrows():
         mr = DA.parse_diligent_da(src_inst['mr']).to_cambridge_da_string()
-        sys = [{'sys': src_inst['sys%d' % i],
-                'ref': src_inst['ref%d' % i],
-                'val': src_inst['%s%d' % (src_col, i)]} for i in xrange(1, 6)]
+        delex_mr = DA.parse_diligent_da(src_inst['mr']).get_delexicalized(set(['name', 'near'])).to_cambridge_da_string()
+        unique_mrs.add(delex_mr)
+        syss = [{'sys': src_inst['sys%d' % i],
+                 'ref': src_inst['ref%d' % i],
+                 'val': src_inst['%s%d' % (src_col, i)]} for i in xrange(1, 6)]
 
-        for sys1, sys2 in itertools.combinations(sys, 2):
+        for sys1, sys2 in itertools.combinations(syss, 2):
             if sys1['val'] < sys2['val']:  # without loss of generality
                 sys1, sys2 = sys2, sys1
             if sys1['val'] == sys2['val']:  # ignore those that are equal
@@ -73,6 +81,7 @@ def convert(args):
                         'system2': SYSTEMS_MAP[sys2['sys']],
                         'orig_ref': None,
                         'mr': mr,
+                        'delex_mr': delex_mr,
                         'system_ref': sys1['ref'],
                         'system_ref2': sys2['ref'],
                         'is_real': 1,
@@ -82,15 +91,50 @@ def convert(args):
             trg_inst[trg_col] = 1
             data.append(trg_inst)
 
-    df = pd.DataFrame(data)
-    df.to_csv(args.out_file, columns=COLUMNS, sep=b"\t", index=False, encoding='UTF-8')
+    unique_mrs = sorted(list(unique_mrs))
+    random.shuffle(unique_mrs)
+
+    part_sizes = [int(p) for p in args.ratio.split(':')]
+    part_sizes = [int(round(p * len(unique_mrs) / float(sum(part_sizes)))) for p in part_sizes]
+    part_sizes[0] = len(unique_mrs) - sum(part_sizes[1:])
+    part_labels = args.labels.split(':')
+    part_start = 0
+    print >> sys.stderr, 'Data sizes in MRs: %s' % ':'.join([str(p) for p in part_sizes])
+
+    # mark down the configuration
+    with codecs.open(os.path.join(args.out_path, 'config'), 'wb', encoding='UTF-8') as fh:
+        fh.write(pprint.pformat(vars(args), indent=4, width=100))
+
+    # split the output
+    for part_size, part_label in zip(part_sizes, part_labels):
+        part_mrs = set(unique_mrs[part_start: part_start + part_size])
+        part_data = [inst for inst in data if inst['delex_mr'] in part_mrs]
+
+        if args.shuffle:
+            random.shuffle(part_data)
+
+        part_df = pd.DataFrame(part_data)
+
+        out_file = os.path.join(args.out_path, part_label + '.tsv')
+        print >> sys.stderr, 'File: %s, total size %d' % (out_file, len(part_data))
+        part_df.to_csv(out_file, columns=COLUMNS, sep=b"\t", index=False, encoding='UTF-8')
+
+        part_start += part_size
 
 
 if __name__ == '__main__':
     ap = ArgumentParser(description='Convert E2E data to our format.')
     ap.add_argument('--column', '-d', type=str, help='Column to use (others will be NaN)', required=True)
+    ap.add_argument('-s', '--shuffle', action='store_true',
+                    help='Shuffle data (within sets)')
+    ap.add_argument('-r', '--ratio', type=str, default='8:1:1',
+                    help='Train-devel-test split ratio (counted in unique MRs)')
+    ap.add_argument('-l', '--labels', type=str, default='train:devel:test',
+                    help='Train-devel-test labels (default: train:devel:test)')
+
     ap.add_argument('src_file', type=str, help='Path to source data file')
-    ap.add_argument('out_file', type=str, help='Output TSV file')
+    ap.add_argument('out_path', type=str, help='Output TSV file path (will be concat with <label>.tsv)')
 
     args = ap.parse_args()
+    random.seed(1206)
     convert(args)
---
+++ experiments/input/create_v4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import errno
+import os
+import pandas as pd
+import sys
+
+def concat_sets(src1, src2, dst, shorten=None):
+    for part in ['train.tsv', 'devel.tsv', 'test.tsv']:
+        print >> sys.stderr, ('%s + %s -> %s (%s)' % (src1, src2, dst, part))
+        data = pd.read_csv(os.path.join(src1, part), sep='\t', encoding='UTF-8', index_col=None)
+        add = pd.read_csv(os.path.join(src2, part), sep='\t', encoding='UTF-8', index_col=None)
+        if shorten and len(add) > shorten:  # use less data
+            add = add.sample(n=shorten)
+        data = data.append(add)
+        data.to_csv(os.path.join(dst, part), columns=data.columns, sep=b"\t", index=False, encoding='UTF-8')
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+## make directories
+#mkdir_p('../data/v4/e2e_qua')
+#mkdir_p('../data/v4/e2e_nat')
+#mkdir_p('../data/v4/e2e_both')
+
+## basic data conversion
+#os.system('./convert_e2e.py --shuffle --column quality nlg-datasets/quality-fixed_utf.csv ../data/v4/e2e_qua/')
+#os.system('./convert_e2e.py --shuffle --column natur nlg-datasets/naturalness-fixed_utf.csv ../data/v4/e2e_nat/')
+
+## concat version
+#concat_sets('../data/v4/e2e_qua/', '../data/v4/e2e_nat/', '../data/v4/e2e_both/')
+
+# with ratings data
+for cvnum in ['cv00', 'cv01', 'cv02', 'cv03', 'cv04']:
+    mkdir_p('../data/v4cv/joint/%s' % cvnum)
+    concat_sets('../data/v3cv/noref/%s' % cvnum, '../data/v4/e2e_both/', '../data/v4cv/joint/%s' % cvnum)
+
+    mkdir_p('../data/v4cv/joint_small/%s' % cvnum)
+    concat_sets('../data/v3cv/noref/%s' % cvnum, '../data/v4/e2e_both/', '../data/v4cv/joint_small/%s' % cvnum, shorten=5000)
+
+    mkdir_p('../data/v4cv/joint_Ftonly/%s' % cvnum)
+    concat_sets('../data/v3cv/noref_Ftonly/%s' % cvnum, '../data/v4/e2e_both/', '../data/v4cv/joint_Ftonly/%s' % cvnum)
